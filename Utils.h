f8b5928bf30afd02b18da778df623442111bd007
bk20dev <itsbk20@gmail.com>
2022-10-30 00:28:09 +0200

    Add text search (ignore case) util

---
+++ Utils.h
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#ifndef UtilsH
+#define UtilsH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+//---------------------------------------------------------------------------
+bool containsIgnoreCase(UnicodeString haystack, UnicodeString needle) {
+	auto haystackUpper = haystack.UpperCase();
+	auto needleUpper = needle.UpperCase();
+
+	UnicodeString::iterator iter = std::search(
+		haystackUpper.begin(), haystackUpper.end(),
+		needleUpper.begin(), needleUpper.end()
+	);
+	return iter != haystackUpper.end();
+}
+//---------------------------------------------------------------------------
+#endif UtilsH
